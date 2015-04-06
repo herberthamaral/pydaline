89fdd9b921ce108dea4be1315edacc7ee1eb954f
Herberth Amaral <herberth.amaral@sisqualis.com.br>
2015-04-06 16:12:01 -0300

    Faz projeto prático 1

---
+++ projetopratico1.py
@@ -0,0 +1,63 @@
+# encoding: utf-8
+import os
+import sys
+import re
+from pydaline import Pydaline
+
+dataset = [[float(coluna) for coluna in linha.split(' ')] for linha in open('dados.txt').read().split('\n')[1:] if linha!='']
+pesos_set = [[float(coluna) for coluna in linha.split(' ')] for linha in open('pesos.txt').read().split('\n') if linha!='']
+dataset_questao3 = [[float(coluna) for coluna in linha.split(' ')] for linha in open('questao3.txt').read().split('\n') if linha!='']
+
+entradas = [(linha[0], linha[1], linha[2]) for linha in dataset if len(linha)==4]
+saidas_desejadas = [linha[3] for linha in dataset if len(linha)==4]
+
+#plotagem
+def plot():
+    try:
+        from mpl_toolkits.mplot3d import Axes3D
+        import matplotlib.pyplot as plt
+    except ImportError:
+        os.system('pip install matplotlib')
+        plot()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    pureza_tipo1_x = [linha[0] for linha in dataset if int(linha[3]) == 0]
+    pureza_tipo1_y = [linha[1] for linha in dataset if int(linha[3]) == 0]
+    pureza_tipo1_z = [linha[2] for linha in dataset if int(linha[3]) == 0]
+
+    pureza_tipo2_x = [linha[0] for linha in dataset if int(linha[3]) == 1]
+    pureza_tipo2_y = [linha[1] for linha in dataset if int(linha[3]) == 1]
+    pureza_tipo2_z = [linha[2] for linha in dataset if int(linha[3]) == 1]
+
+    ax.scatter(pureza_tipo1_x, pureza_tipo1_y, pureza_tipo1_z, c='r', marker='o')
+    ax.scatter(pureza_tipo2_x, pureza_tipo2_y, pureza_tipo2_z, c='r', marker='x')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
+def exercicio_2():
+    neuronio = Pydaline()
+    for i, amostra in enumerate(dataset_questao3):
+        print "\nAmostra %d"%(i+1)
+        for treinamento in pesos_set:
+            neuronio.pesos = treinamento 
+            sys.stdout.write(str(neuronio.get_saida(dataset_questao3[i]))+" ")
+
+def exercicio_1():
+    neuronio = Pydaline()
+    neuronio.set_entradas(entradas)
+    neuronio.set_saidas_desejadas(saidas_desejadas)
+    neuronio.set_debug(False)
+    neuronio.aprender()
+
+if __name__=='__main__':
+    if 'plot' in sys.argv:
+        plot()
+    elif 'ex1' in sys.argv:
+        exercicio_1()
+    else:
+        exercicio_2()
--- pydaline.py
+++ pydaline.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
+# encoding: utf-8
 import random
 random.randrange(start=-1,stop=1)
-class Pydaline:
+
+class Pydaline(object):
     
     def __init__(self):
         """
@@ -10,56 +11,65 @@ class Pydaline:
         self.pesos = []
         self.saidas_desejadas = []
         self.entradas = []
-        self.theta = 0.7 # inteligente bagaraio
-        self.alpha = 0.9 #taxa de aprendizado
+        self.taxa_aprendizado = 0.01
         self.debug = True
+        self.theta = .7
         self.epocas = 0
-        self.erroGlobal = 1 # assume que inicia sem treinamento
+        self.erro_global = 1
 
-    def setDebug(self,debug=True):
+    def set_debug(self,debug=True):
         """
         Se ativado, ele mostrará as mensagens de treinamento
         """
         self.debug = debug
         
-    def setEntradas(self,entradas):
+    def set_entradas(self,entradas):
         """
         Aplica as entradas fornecidas e reajusta o número de pesos
         """
-        if entradas.__len__()>=2:
+        if len(entradas) >= 2:
             #cria novos pesos
             self.entradas = entradas
             self.pesos = []
-            for i in range(0,entradas[0].__len__()):
+            for i, entrada in enumerate(entradas[0]):
                 self.pesos.append(random.random())
 
-    def setSaidasDesejadas(self,saidas):
+    def get_saida(self, entrada):
+        if not self.pesos:
+            raise RuntimeError(u'Pesos não configurados')
+        if len(self.pesos) != len(entrada):
+            raise RuntimeError(u'Vetor de pesos diferente do vetor de entrada')
+
+        self.entradas = [entrada]
+        return self.ativacao(0)
+
+    def set_saidas_desejadas(self,saidas):
         """
         Ajusta as as saídas desejadas necessárias para treinar o neuronio
         """
-        if saidas.__len__()>=2:
+        if len(saidas) >= 2:
             self.saidas_desejadas = saidas
 
     def criterio_parada(self):
         """
         Critério de parada. Sobrescreva para mudar.
         """
-        return (self.epocas==100) #treinamento máximo em 100 épocas
+        return (self.epocas==100)
     
-    def somatorio(self,numEntrada):
+    def somatorio(self, num_entrada):
         """
         Faz o somatório do produto de cada entrada com seu respectivo peso
         """
         somatorio = 0
-        for i in range(0,self.entradas[numEntrada].__len__()):
-            somatorio += self.pesos[i]*self.entradas[numEntrada][i]
+        for i,entrada in enumerate(self.entradas[num_entrada]):
+            somatorio += self.pesos[i]* entrada
         return somatorio
     
-    def ativacao(self,numEntrada):
+    def ativacao(self, num_entrada):
         """
         Função de ativação. Sobrescreva se quiser uma diferente.
         """
-        somatorio = self.somatorio(numEntrada)
+        somatorio = self.somatorio(num_entrada)
         if somatorio>=self.theta:
             return 1
         elif somatorio<-self.theta:
@@ -70,12 +80,16 @@ class Pydaline:
         """
         Realiza o treinamento por épocas.
         """
-        if self.saidas_desejadas.__len__()!=self.entradas.__len__():
+        if len(self.saidas_desejadas) != len(self.entradas):
             raise RuntimeError("Whoops! O número de entradas é diferente do número de saídas desejadas.")
-        while self.erroGlobal != 0 and not self.criterio_parada():
+        while self.erro_global != 0:
+            if self.criterio_parada():
+                print "Não convergiu. Critério de parada atingido. Pesos:", self.pesos, "Erro global:", self.erro_global
+                break
             self.epocas += 1
-            if self.debug: print "Iniciando epoca: ",self.epocas,". Erro Global:",self.erroGlobal
-            self.erroGlobal = 0
+            if self.debug: 
+                print "Iniciando epoca: ",self.epocas,". Erro Global:",self.erro_global
+            self.erro_global = 0
             self.treina()
             
             
@@ -83,36 +97,30 @@ class Pydaline:
         """
         Percorre as entradas, calcula as saídas e passa o erro para a função de ajuste
         """
-        for i in range(0,self.entradas.__len__()): #treina o nuronio para cada entrada
-            ativacao = self.ativacao(i)
-            erro = self.saidas_desejadas[i]-ativacao
-            self.erroGlobal += erro.__abs__()
+        for i, entrada in enumerate(self.entradas): #treina o nuronio para cada entrada
+            saida = self.ativacao(i)
+            erro = self.saidas_desejadas[i]-saida
+            self.erro_global += abs(erro)
             if self.debug:
-                print " Entradas",self.entradas[i]
+                print "\n Entradas",entrada
                 print " Saida desejada: ",self.saidas_desejadas[i]
-                print " Saida encontrada: ",ativacao
+                print " Saida encontrada: ",saida
                 print " Erro: ",erro
                 print " Pesos:",self.pesos
-            self.ajusta_pesos(erro,i)
+            self.ajusta_pesos(erro, i)
 
-    def ajusta_pesos(self,erro,numEntrada):
+    def ajusta_pesos(self, erro, num_entrada):
         """
         Principal função: realiza o ajuste de pesos (aprendizado) quando houver erro.
         """
         if erro !=0:
-            plen = self.pesos.__len__()
+            plen = len(self.pesos)
             for j in range(0,plen):
-                self.pesos[j] = self.pesos[j] + self.alpha*erro*self.entradas[numEntrada][j]
-            if self.debug: print " Pesos atualizados:",self.pesos
+                x = self.entradas[num_entrada][j]
+                self.pesos[j] = self.pesos[j] + self.taxa_aprendizado*erro*x
+            if self.debug: 
+                print " Pesos atualizados:",self.pesos
     
-    def obter_resposta_para_entrada(self, entrada):
-        if entrada.__len__() != self.entradas[0].__len__():
-            raise RuntimeError("A entrada deve ter o mesmo tamanho das usadas no treinamento")
-        self.entradas.append(entrada)
-        resultado = self.ativacao(self.entradas.__len__()-1)
-        self.entradas.pop()
-        return resultado
-
     def teste_manual(self):
         """
         Teste individual
@@ -121,262 +129,11 @@ class Pydaline:
         r = "s"
         entradas_fornecidas = []
         while r=="s":
-            for i in range(self.entradas[0].__len__()): # for(i=0;i<numEntradas;i++)
+            for i in range(len(self.entradas[0])):
                 print "Entrada ",i
-                entradas_fornecidas.append(int(raw_input(": ")))
+                entradas_fornecidas.append(float(raw_input(": ")))
             self.entradas[0] = entradas_fornecidas
             if self.debug: print entradas_fornecidas
             print "Saida: ",self.ativacao(0)
             r = raw_input("Continuar testando?")
             entradas_fornecidas = []
-
-
-def main():
-    ##############################################################
-    digito_a0 = [1,1,1,1,1,
-                 1,1,0,1,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1,
-                 1,0,0,0,1]
-
-    digito_a1 =  [0,1,1,1,0,
-                  1,1,0,1,1,
-                  1,0,0,0,1,
-                  1,1,1,1,1,
-                  1,0,0,0,1]
-
-    digito_b0 = [1,1,1,1,0,
-                 1,0,0,0,1,
-                 1,1,1,1,1,
-                 1,0,0,0,1,
-                 1,1,1,1,0]
-
-    digito_b1 = [1,1,1,1,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1]
-
-    digito_c0 = [1,1,1,1,1,
-                 1,0,0,0,0,
-                 1,0,0,0,0,
-                 1,0,0,0,0,
-                 1,1,1,1,1]
-
-    digito_c1 = [0,1,1,1,1,
-                 1,1,0,0,0,
-                 1,0,0,0,0,
-                 1,1,0,0,0,
-                 0,1,1,1,1]
-
-    digito_d0 = [1,1,1,1,0,
-                 1,0,0,1,1,
-                 1,0,0,0,1,
-                 1,0,0,1,1,
-                 1,1,1,1,0]
-
-    digito_d1 = [1,1,1,1,1,
-                 1,0,0,1,1,
-                 1,0,0,0,1,
-                 1,0,0,1,1,
-                 1,1,1,1,1]
-
-    digito_e0 = [1,1,1,1,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1,
-                 1,0,0,0,0,
-                 1,1,1,1,1]
-
-    digito_e1 = [1,1,1,1,0,
-                 1,0,0,0,1,
-                 1,1,1,1,0,
-                 1,0,0,0,0,
-                 1,1,1,1,0]
-
-    digito_f0 = [1,1,1,1,1,
-                 1,0,0,0,0,
-                 1,1,1,1,1,
-                 1,0,0,0,0,
-                 1,0,0,0,0]
-
-    digito_f1 = [1,1,1,1,1,
-                 1,0,0,0,0,
-                 1,0,0,0,0,
-                 1,1,1,1,1,
-                 1,0,0,0,0]
-
-    digito_g0 = [1,1,1,1,1,
-                 1,0,0,0,0,
-                 1,1,1,1,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1]
-
-    digito_g1 = [1,1,1,1,0,
-                 1,0,0,0,0,
-                 1,1,1,1,0,
-                 1,0,0,0,1,
-                 0,1,1,1,0]
-
-    digito_h0 = [1,1,1,1,1,
-                 1,0,0,0,0,
-                 1,1,1,1,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1]
-
-    digito_h1 = [1,0,0,0,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1,
-                 1,0,0,0,1,
-                 0,1,1,1,1]
-
-    digito_h1 = [1,0,0,0,1,
-                 1,0,0,0,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1,
-                 0,1,1,1,1]
-
-    digito_i0 = [1,1,1,1,1,
-                 0,0,1,0,0,
-                 0,0,1,0,0,
-                 0,0,1,0,0,
-                 1,1,1,1,1]
-
-    digito_i1 = [0,0,1,0,0,
-                 0,0,0,0,0,
-                 0,0,1,0,0,
-                 0,0,1,0,0,
-                 0,1,1,1,0]
-
-    digito_j0 = [0,0,1,1,1,
-                 0,0,0,0,1,
-                 1,0,0,0,1,
-                 1,0,0,0,1,
-                 1,1,1,1,1]
-
-    digito_j1 = [0,0,0,0,1,
-                 0,0,0,0,0,
-                 0,0,0,0,1,
-                 0,1,0,0,1,
-                 0,1,1,1,1]
-
-    digito_k0 = [1,0,0,1,1,
-                 1,0,1,0,0,
-                 1,1,0,0,0,
-                 1,0,1,0,0,
-                 1,0,0,1,1]
-
-    digito_k1 = [0,0,0,0,0,
-                 1,0,1,0,0,
-                 1,1,0,0,0,
-                 1,0,1,0,0,
-                 1,0,0,1,0]
-    # a = [-1,-1,-1,-1] => neuron_1 = -1, neuron_2 =-1, neuron_3 = -1, etc
-    # b = [-1,-1,-1, 1]
-    # c = [-1,-1, 1,-1]
-    # d = [-1,-1, 1, 1]
-    # e = [-1, 1,-1,-1]
-    # f = [-1, 1,-1, 1]
-    # g = [-1, 1, 1,-1]
-    # h = [-1, 1, 1, 1]
-    # i = [ 1,-1,-1,-1]
-    # j = [ 1,-1,-1, 1]
-    # k = [ 1,-1, 1,-1]
-    # l = [ 1,-1, 1, 1]
-    # m = [ 1, 1,-1,-1]
-    # n = [ 1, 1,-1, 1]
-    # o = [ 1, 1, 1,-1]
-    # p = [ 1, 1, 1, 1]
-
-    entradas = [digito_a0,digito_a1,
-            digito_b0,digito_b1,
-            digito_c0,digito_c1,
-            digito_d0,digito_d1,
-            digito_e0,digito_e1,
-            digito_f0,digito_f1,
-            digito_g0,digito_g1,
-            digito_h1,digito_h1,
-            digito_i0,digito_i1,
-            digito_j0,digito_j1,
-            digito_k0,digito_k1]
-
-    saidas_1 = [-1,-1, #a
-            -1,-1, #b
-            -1,-1, #c
-            -1,-1, #d
-            -1,-1, #e
-            -1,-1,
-            -1,-1,
-            -1,-1,
-             1, 1, #i
-             1, 1,
-             1, 1]
-
-    saidas_2 = [-1,-1,
-            -1,-1,
-            -1,-1,
-            -1,-1,
-             1, 1,
-             1, 1,
-             1, 1,
-             1, 1,
-            -1,-1,
-            -1,-1,
-            -1,-1]
-
-    saidas_3 = [-1,-1,
-            -1,-1,
-             1, 1,
-             1, 1,
-            -1,-1,
-            -1,-1,
-             1, 1,
-             1, 1,
-            -1,-1,
-            -1,-1,
-             1, 1]
-
-    saidas_4 = [-1,-1,
-             1, 1,
-            -1,-1,
-             1, 1,
-            -1,-1,
-             1, 1,
-            -1,-1,
-             1, 1,
-            -1,-1,
-             1, 1,
-            -1,-1]
-
-
-    neuron_1 = Pydaline()
-    neuron_2 = Pydaline()
-    neuron_3 = Pydaline()
-    neuron_4 = Pydaline()
-
-    neuron_1.setEntradas(entradas)
-    neuron_2.setEntradas(entradas)
-    neuron_3.setEntradas(entradas)
-    neuron_4.setEntradas(entradas)
-
-    neuron_1.setSaidasDesejadas(saidas_1)
-    neuron_2.setSaidasDesejadas(saidas_2)
-    neuron_3.setSaidasDesejadas(saidas_3)
-    neuron_4.setSaidasDesejadas(saidas_4)
-
-    #neuron_1.setDebug(False)
-    #neuron_2.setDebug(False)
-    #neuron_3.setDebug(False)
-    #neuron_4.setDebug(False)
-
-    neuron_1.aprender()
-    neuron_2.aprender()
-    neuron_3.aprender()
-    neuron_4.aprender()
-
-    print "Épocas do neurônio 1:",neuron_1.epocas
-    print "Épocas do neurônio 2:",neuron_2.epocas
-    print "Épocas do neurônio 3:",neuron_3.epocas
-    print "Épocas do neurônio 4:",neuron_4.epocas
-
-if __name__ == "__main__":
-    main()
